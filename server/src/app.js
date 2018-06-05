733c00368a0de816ebc1b06e5218eae6c1e5bae1
Leonard Dalmacio <leonarddalmacio@Aracari-2.local>
2018-06-05 17:33:53 +0800

    To be Continued

--- server/src/app.js
+++ server/src/app.js
@@ -3,15 +3,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const {sequelize} = require('./models/index')
+const config = require('./config/config')
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
-  })
-})
+require('./routes')(app);
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync()
+  .then(()=>{
+    app.listen(config.port|| 8081)
+    console.log(`Server started on port ${config.port}`)
+  })
---
+++ server/src/models/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const config = require('../config/config')
+const db = {};
+
+const sequelize = new Sequelize(
+  config.db.database,
+  config.db.user,
+  config.db.password,
+  config.db.option,
+)
+
+module.exports = db
+
