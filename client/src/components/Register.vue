a91cbd390094c840dd51509ca17aee779aa546ec
Leonard Dalmacio <leonarddalmacio@Aracari-2.local>
2018-06-05 16:57:54 +0800

    Setting up the registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>Register</h1>
+        <br>
+        <input 
+        type="email" 
+        name="email"
+        placeholder="Email" 
+        v-model="email"/>
+        <input 
+        type="password" 
+        name="password"
+        placeholder="Password" 
+        v-model="password"/>
+        <br>
+        <button @click="register">Register</button>
+    </div>
+</template>
+
+<script>
+import AuthenticationService from '../../services/AuthenticationService';
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+        }
+    },
+    methods: {
+        async register() {
+            const response = await AuthenticationService.register({
+                email: this.email,
+                password: this.password
+            })
+            console.log(response.data);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,3 @@
-console.log('hello')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -10,9 +8,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
   })
 })
 
